9362a9ff411684c8563cb5335ebe4853447c8c36
IDcLuc <lucativio@gmail.com>
2022-07-14 14:44:56 +0200

    Added more stats

--- commands/stats/bw.js
+++ commands/stats/bw.js
@@ -23,17 +23,19 @@ module.exports = {
 
         let embed = new MessageEmbed() 
             .setTitle(`${playerObj}'s Bedwars Stats`)
-            .setThumbnail(`https://crafatar.com/renders/body/${playerObj.uuid}?overlay&size=128`)
+            .setThumbnail(`https://crafatar.com/renders/body/${playerObj.uuid}?overlay&size=256`)
             .addFields(
-                { name: "Level", value: bedWarsStats.level.toString(), inline: true },
-                { name: "Kills", value: bedWarsStats.kills.toString(), inline: true },
-                { name: "Wins", value: bedWarsStats.wins.toString(), inline: true},
-                { name: "Losses", value: bedWarsStats.losses.toString(), inline: true },
-                { name: "Deaths", value: bedWarsStats.deaths.toString(), inline: true },
-                { name: "Coins", value: bedWarsStats.coins.toString(), inline: true },
-                { name: "K/D Ratio", value: bedWarsStats.KDRatio.toString(), inline: true},
-                { name: "FK/D Ratio", value: bedWarsStats.finalKDRatio.toString(), inline: true},
-                { name: "W/L Ratio", value: bedWarsStats.WLRatio.toString(), inline: true},
+                { name: "Level", value: new Intl.NumberFormat("en-US").format(bedWarsStats.level), inline: true },
+                { name: "Coins", value: new Intl.NumberFormat("en-US").format(bedWarsStats.coins), inline: true },
+                { name: "Kills", value: new Intl.NumberFormat("en-US").format(bedWarsStats.kills) , inline: true },
+                { name: "Deaths", value: new Intl.NumberFormat("en-US").format(bedWarsStats.deaths), inline: true },
+                { name: "Final Kills", value: new Intl.NumberFormat("en-US").format(bedWarsStats.finalKills), inline: true },
+                { name: "Final Deaths", value: new Intl.NumberFormat("en-US").format(bedWarsStats.finalDeaths), inline: true },
+                { name: "Wins", value: new Intl.NumberFormat("en-US").format(bedWarsStats.wins) , inline: true },
+                { name: "Losses", value: new Intl.NumberFormat("en-US").format(bedWarsStats.losses), inline: true },
+                { name: "K/D Ratio", value: new Intl.NumberFormat("en-US").format(bedWarsStats.KDRatio) , inline: true },
+                { name: "FK/D Ratio", value: new Intl.NumberFormat("en-US").format(bedWarsStats.finalKDRatio), inline: true},
+                { name: "W/L Ratio", value: new Intl.NumberFormat("en-US").format(bedWarsStats.WLRatio), inline: true},
             )
             .setFooter({ text: `Galactic Bot Stats ● Requested by ${message.author.tag}`, iconURL: client.user.displayAvatarURL() })
 
