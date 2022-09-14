c28e996c77c945fd5c9db4dbfeddc256748b5f36
IDcLuc <62775328+IDcLuc@users.noreply.github.com>
2022-09-14 22:45:47 +0200

    fix indenting

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -18,24 +18,24 @@ module.exports = {
 
         if (!command) return
 
-    if(!cooldowns.has(command.name)){
-        cooldowns.set(command.name, new Discord.Collection())
-    }       
+        if(!cooldowns.has(command.name)){
+            cooldowns.set(command.name, new Discord.Collection())
+        }       
 
-    const now = Date.now()
-    const timestamps = cooldowns.get(command.name)
-    const cooldownAmount = (command.cooldown) * 1000
+        const now = Date.now()
+        const timestamps = cooldowns.get(command.name)
+        const cooldownAmount = (command.cooldown) * 1000
 
-    if(timestamps.has(message.author.id && !owners.includes(message.author.id))){
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount
+        if(timestamps.has(message.author.id && !owners.includes(message.author.id))){
+            const expirationTime = timestamps.get(message.author.id) + cooldownAmount
 
-        if(now < expirationTime){
-            const timeLeft = (expirationTime - now) / 1000
-            return message.reply(`Please wait ${timeLeft.toFixed(1)} more seconds to use this command.`)
+            if(now < expirationTime){
+                const timeLeft = (expirationTime - now) / 1000
+                return message.reply(`Please wait ${timeLeft.toFixed(1)} more seconds to use this command.`)
+            }
         }
-    }
 
-    timestamps.set(message.author.id, now)
+        timestamps.set(message.author.id, now)
 
         let member = message.member
 
