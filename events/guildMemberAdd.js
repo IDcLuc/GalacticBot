8cb14dd41953874f01583ac5580d12937041bb7b
IDcLuc <62775328+IDcLuc@users.noreply.github.com>
2022-10-20 23:34:45 +0200

    Move guildmemberadd to a dedicated file

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,7 @@ module.export = {
     name: "guildMemberAdd"
     run: async (member) => {
         const img = await imageGenerator(member)
+        const welcomeChannelId = '986310622876676136'
         member.guild.channels.cache.get(welcomeChannelId).send({
             content: `Hey, <@${member.id}>! Welcome to the server, please make sure to read the rules before chatting!`,
             files: [img]
